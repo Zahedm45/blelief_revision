992c900047215ded9b4bc82902943a91a89f95e7
Zahed <zahedm45@gmail.com>
2022-04-17 22:39:10 +0200

    Updating on git

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utility import find_prop
 
-IMPLICATION = "="
+IMPLICATION = "=>"
+BICONDITION = "<=>"
 
 
 class CNF:
@@ -9,18 +10,39 @@ class CNF:
         for i in range(len(proposition) - 1):
             if proposition[i] == "=" and proposition[i + 1] == ">":
                 old_val = self.find_proposition(proposition, i)
-                new_val = self.remove_implication(old_val)
+                new_val = self.implication(old_val)
                 proposition = self.replace_proposition(old_val, new_val, proposition)
-                print("after: ",proposition)
-                #break
+                print("After implication: ",proposition)
 
-    def remove_implication(self, proposition):
+                #break
+            # if proposition[i] == "<" and proposition[i+1] == "=":
+            #     if proposition[i+2] == ">":
+            #         old_val = self.find_proposition(proposition, i)
+            #         new_val = self.biconditional(old_val)
+            #         print(new_val)
+
+
+    def implication(self, proposition):
         proposition = proposition.replace("=", "V", 1)
         proposition = proposition.replace(">", "", 1)
-        return "!" + proposition
+        return "~" + proposition
         # print(proposition)
 
-    #def remove_biconditional(self):
+
+    def biconditional(self, propos):
+        #print(propos)
+        p = q = ""
+        for i in range(len(propos)-3):
+            str = propos[i]
+            val = str +propos[i+1]+propos[i+2]
+
+            if val == BICONDITION:
+                q += propos.replace(p+"<=>", "")
+                break
+            else:
+                p += str
+        print(p," ",q)
+
 
 
 
@@ -35,7 +57,9 @@ class CNF:
 cnf = CNF()
 str = "(pVq)=>(!m)^(b)=>(h)"
 
+##str = "((pVc)<=>(qVm))"
 
 
-print("before: " + str)
+
+print("Before implication: " + str)
 cnf.convert_to_cnf(str)
